e0e4e1ddfca74865cfdf95b0d5e6cb17a4adebc4
victorheld <victor@xirion.net>
2016-08-18 22:03:32 +0200

    Added lock.service to install/remove scripts

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-if [[ $UID != 0 ]]; 
+if [[ $UID != 0 ]];
 then
     sudo cp ./{lock,lock.png,lockdark.png} /usr/local/bin/
+    sudo cp ./lock.service /usr/lib/systemd/system/
 else
     cp ./{lock,lock.png,lockdark.png} /usr/local/bin/
+    cp ./lock.service /usr/lib/systemd/system/
 fi
--- remove.sh
+++ remove.sh
@@ -2,6 +2,8 @@
 if [[ $UID != 0 ]]; 
 then
     sudo rm /usr/local/bin/{lock,lock.png,lockdark.png} 
+    sudo rm /usr/lib/systemd/system/lock.service
 else
     rm /usr/local/bin/{lock,lock.png,lockdark.png}
+    rm /usr/lib/systemd/system/lock.service
 fi
